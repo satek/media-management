11c346e0025fddfeb356648898596db8549fdb1b
Marko Satek <satekm@gmail.com>
2015-04-28 06:49:04 +0200

    video items controller routes

--- app/controllers/video_items_controller.rb
+++ app/controllers/video_items_controller.rb
@@ -1,7 +1,32 @@
 class VideoItemsController < ApplicationController
+  before_action :set_video_item, only: [:show, :edit, :update]
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @item.update(video_item_params)
+        format.html {
+          redirect_to @item, notice: "Item was successfully updated"
+        }
+      else
+        format.html
+      end
+    end
+  end
+
+  private
+
+  def set_video_item
     @item = VideoItem.find params['id']
+  end
+
+  def video_item_params
+    params['video_item'].permit(:title, :description)
   end
 
 end
---
+++ spec/controllers/video_items_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+
+RSpec.describe VideoItemsController, type: :controller do
+
+  before(:each) do
+    @video_item = FactoryGirl.create :video_item
+  end
+  
+  describe "GET show" do
+    it "assigns @item" do
+      get :show, id: @video_item.id
+      assigned = assigns(:item)
+      expect(assigned.title).to eq(@video_item.title)
+      expect(assigned.description).to eq(@video_item.description)
+    end
+  end
+
+  describe "GET edit" do
+    it "assigns @item" do
+      get :edit, id: @video_item.id
+      assigned = assigns(:item)
+      expect(assigned.title).to eq(@video_item.title)
+      expect(assigned.description).to eq(@video_item.description)
+    end
+  end
+
+  describe "PUT update" do
+    it "assigns and updates @item" do
+      new_attrs = FactoryGirl.attributes_for :video_item
+      put :update, id: @video_item.id, video_item: new_attrs.except(:type, :user_id)
+      @video_item.reload
+      expect(@video_item.title).to eq(new_attrs[:title])
+      expect(@video_item.description).to eq(new_attrs[:description])
+      expect(response).to redirect_to(video_item_path(@video_item.id))
+    end
+  end
+end
