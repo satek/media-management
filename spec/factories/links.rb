378e139466c76e015e63e2bdf3a2ce5195aed236
Marko Satek <satekm@gmail.com>
2015-04-28 14:20:27 +0200

    removed old specs and factories

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,15 +1,5 @@
 FactoryGirl.define do
   factory :link do
     url { Faker::Internet.url }
-
-    factory :video_link do
-      type "VideoLink"
-      media_item_id { FactoryGirl.create(:video_item).id }
-    end
-
-    factory :web_link do
-      type "WebLink"
-      media_item_id { FactoryGirl.create(:link_item).id }
-    end
   end
 end
