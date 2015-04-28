1f7c9e64d63fb34fc8d009832da9f51c9e581810
Marko Satek <satekm@gmail.com>
2015-04-28 07:23:35 +0200

    modified resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
 
-  resources :media_items
-
-  resources :video_items, only: [:show, :new, :edit, :create] do
+  resources :media_items do
     resources :links
   end
 
-  resources :link_items, only: [:show, :new, :edit, :create]
+  resources :video_items, only: [:show, :edit, :update]
 
-  resources :web_items, only: [:show, :new, :edit, :create] do
-    resources :links
-  end
+  resources :link_items, only: [:show, :edit, :update]
+
+  resources :image_items, only: [:show, :edit, :update]
 
 end
